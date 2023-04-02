69b5b076a7ba637f6a4461095560af54735b4886
Tun Putera Matin <tunputeramatin@TPM-MacBook-Air.local>
2023-04-02 15:28:07 +0800

    new js

---
+++ index.js
@@ -0,0 +1,5 @@
+let num1 = document.getElementById('num1').innerHTML;
+let num2 = document.getElementById('num2').innerHTML;
+let num3 = document.getElementById('num3').innerHTML;
+let num4 = document.getElementById('num4').innerHTML;
+
