abc8ee0890609eaa750b1f44160cdd544ac3727a
Tun Putera Matin <tunputeramatin@TPM-MacBook-Air.local>
2023-04-03 00:09:45 +0800

    changes in js & html

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let num1 = document.getElementById('num1').innerHTML;
-let num2 = document.getElementById('num2').innerHTML;
-let num3 = document.getElementById('num3').innerHTML;
-let num4 = document.getElementById('num4').innerHTML;
+
+let num1 = Number(document.getElementById('num1').innerHTML);
+let num2 = Number(document.getElementById('num2').innerHTML);
+let num3 = Number(document.getElementById('num3').innerHTML);
+let num4 = Number(document.getElementById('num4').innerHTML);
+
+document.getElementById("total_value").textContent =
+num1 + num2 + num3 + num4;
+
+let totalSum = num1 + num2 + num3 + num4;
+
+couponDiscount = function (){
+ document.getElementById("total_value").textContent = totalSum * 0.8;
+}
 
